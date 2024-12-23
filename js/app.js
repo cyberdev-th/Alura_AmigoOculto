e7891ebcdcda582e1b4e682a1e42cb9722ca911e
Thiago Fernandes <devcyber.thiago@gmail.com>
2024-12-23 13:41:24 -0300

    Validações de entradas de dados

--- js/app.js
+++ js/app.js
@@ -1,33 +1,46 @@
 let listaAmigos = document.getElementById("lista-amigos");
 let nomes = [];
+let nomes2 = [];
 let listaSorteio = document.getElementById("lista-sorteio");
 
 function adicionar() {
+  let nomeAmigo = document.getElementById("nome-amigo");
+  if (nomes.includes(nomeAmigo.value) || nomeAmigo.value.trim() === "") {
+    alert("Nome repetido ou vazio. Tente outro!");
+    nomeAmigo.value = "";
+    return;
+  }
   if (nomes.length !== 0) {
     listaAmigos.textContent += ", ";
   }
-  let nomeAmigo = document.getElementById("nome-amigo");
   listaAmigos.textContent += `${nomeAmigo.value}`;
   nomes.push(nomeAmigo.value);
+  nomes2.push(nomeAmigo.value);
   nomeAmigo.value = "";
 }
 
 function sortear() {
-  if (nomes.length % 2 !== 0) {
+  if (nomes.length < 2) {
     alert(
-      "Impossível sortear amigos em números ímpar, adicione ou retire alguém da lista."
+      "Impossível sortear amigos em números ímpar, adicione alguém na lista."
     );
     return;
   }
   while (nomes.length !== 0) {
     let amigo1 = nomes.pop();
-    let indice = Math.floor(Math.random() * nomes.length);
-    let amigo2 = nomes.splice(indice, 1);
+    let amigo2;
+    while (true) {
+      let indice = Math.floor(Math.random() * nomes2.length);
+      if (nomes2[indice] !== amigo1) {
+        amigo2 = nomes2.splice(indice, 1)[0];
+        break;
+      }
+    }
     listaSorteio.innerHTML += `${amigo1} --> ${amigo2}<br>`;
   }
 }
 
 function reiniciar() {
-    listaAmigos.textContent = '';
-    listaSorteio.innerHTML = '';
+  listaAmigos.textContent = "";
+  listaSorteio.innerHTML = "";
 }
